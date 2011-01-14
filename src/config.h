d8818d701ab159aa9ae23bb6e302e884b9b364f9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-01-14 16:12:05 +0900

    bundle libqrencode

---
+++ src/config.h
@@ -0,0 +1,9 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Version number of package */
+#define VERSION "3.1.1"
+
+/* Define to 'static' if no test programs will be compiled. */
+#define __STATIC static
+   
