788b59b8943590f21b692137c23a0323b4245711
Yoshiki Kurihara <clouder@gmail.com>
2011-01-17 01:31:27 +0900

    changed version

--- lib/Imager/QRCode.pm
+++ lib/Imager/QRCode.pm
@@ -11,7 +11,7 @@ use Carp qw(croak);
 use Imager 0.55;
 
 BEGIN {
-    $VERSION = '0.032';
+    $VERSION = '0.033';
     eval {
         require XSLoader;
         XSLoader::load('Imager::QRCode', $VERSION);
